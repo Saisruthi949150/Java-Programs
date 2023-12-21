db7959924ce43f7f56634d57067d9948c1e56aa2
Saisruthi949150 <154490859+Saisruthi949150@users.noreply.github.com>
2023-12-21 10:55:26 +0530

    Add files via upload

---
+++ CapricornNumber.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class CapricornNumber
+{
+
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number=");
+        int n = scanner.nextInt();
+         boolean isCapricorn = false;
+         
+        int square = n * n;
+        int temp = square;
+        int contDigits = 0;
+        
+        while (temp > 0)
+        {
+            contDigits++;
+            temp /= 10;
+        }
+       
+        for (int i = 1; i < contDigits; i++)
+        {
+            int divisor = (int) Math.pow(10, i);
+            int quotient = square / divisor;
+            int remainder = square % divisor;
+            if (quotient + remainder == n)
+            {
+                isCapricorn = true;
+            }
+        }
+        if (isCapricorn)
+        {
+            System.out.println("Capricorn/Kaprekar number");
+        } else
+        {
+            System.out.println("Not Capricorn/Kaprekar number");
+        }
+    }
+}
